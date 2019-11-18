118aa730fe19b96bee8e58957be147b5bfb9670c
Teoman Kiraz <t.kiraz@outlook.com>
2019-11-18 13:32:30 +0300

    increased readability and did some minor tweaks

--- project_schema.sql
+++ project_schema.sql
@@ -1,131 +1,118 @@
+
 CREATE TABLE PERSON (
-  sno   number(12),
+  sno   number(12) NOT NULL UNIQUE,
   name   varchar(40),
   pass    varchar(8),
   bdate    date,
-  sex      char
+  sex      char,
+PRIMARY KEY (sno)
 );
 
 CREATE TABLE DOCTOR (
-  sno           number(12),
+  sno           number(12)	NOT NULL UNIQUE,
   branch        varchar(25),
   expertise      varchar(25),
-  tax_no       varchar(25)
+  tax_no       varchar(25),
+  PRIMARY KEY (sno),
+  FOREIGN KEY (sno) REFERENCES PERSON(sno) ON DELETE CASCADE
+
 );
 
 CREATE TABLE PRESCRIPTION (
-  sno number(12),
-  pres_no number(10),
+  sno number(12)        NOT NULL,
+  pres_no number(10)    DEFAULT (1) NOT NULL,
   ldtu date,
-  kind varchar(10) 
+  kind varchar(10) ,
+  PRIMARY KEY (sno, pres_no),      
+  FOREIGN KEY (sno) REFERENCES DOCTOR(sno)
 );
 
 CREATE TABLE MEDICINE (
-  bcode      number(13),
+  bcode      number(13)	NOT NULL UNIQUE,
   mname      varchar(25),
   atc_name   varchar(50),
   atc_code  varchar(7),
-  use_type  varchar(15) 
+  use_type  varchar(15),
+PRIMARY KEY (bcode) 
 );
 
 CREATE TABLE ATC_AUTH (
-  sno      number(13),
-  atc_code  varchar(7)
+  sno      number(13)	NOT NULL UNIQUE,
+  atc_code  varchar(7) NOT NULL,
+  PRIMARY KEY (sno, atc_code),
+  FOREIGN KEY (sno) REFERENCES DOCTOR(sno) ON DELETE CASCADE
 );
 
 CREATE TABLE CONTENT (
-  sno   number(12),
-  pres_no    number(10),
-  bcode  decimal(13),
-  amount number(2)
-);
-
-CREATE TABLE PHARMACIST (
-  sno      number(12),
-  tax_no   number(10)
+  sno   number(12)      NOT NULL,
+  pres_no    number(10) NOT NULL,
+  bcode  decimal(13)    NOT NULL,
+  amount number(2),
+FOREIGN KEY (sno) REFERENCES PRESCRIPTION(sno),
+FOREIGN KEY (pres_no) REFERENCES PRESCRIPTION(pres_no) ON  DELETE CASCADE,
+FOREIGN KEY (bcode) REFERENCES MEDICINE(bcode) ON DELETE CASCADE,
+PRIMARY KEY (sno, pres_no, bcode)
 );
 
 CREATE TABLE HEALTH_INSTITUTION (
-  tax_no      number(10),
-  address     varchar(100)
+  tax_no      number(10)	NOT NULL,
+  address     varchar(100),
+PRIMARY KEY (tax_no)
 );
 
 CREATE TABLE PHARMACY (
-  tax_no   number(10),
-  pname varchar(25)
+  tax_no   number(10)	NOT NULL,
+  pname varchar(25),
+  PRIMARY KEY (tax_no)
+  FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no) ON DELETE CASCADE,
 );
 
-CREATE TABLE INVENTORY (
+CREATE TABLE PHARMACIST (
+  sno      number(12)	NOT NULL,
   tax_no   number(10),
-  bcode decimal(13),
-  amount number(3)
+  FOREIGN KEY (sno) REFERENCES PERSON(sno) ON DELETE CASCADE,
+  FOREIGN KEY (tax_no) REFERENCES PHARMACY(tax_no)
+);
+
+
+CREATE TABLE INVENTORY (	
+  tax_no   number(10)	NOT NULL,
+  bcode decimal(13)	NOT NULL,
+  amount number(3),
+  FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no) ON DELETE CASCADE,
+  FOREIGN KEY (bcode) REFERENCES MEDICINE(bcode),
+  PRIMARY KEY (tax_no, bcode)
 );
 
 CREATE TABLE DEPOT (
-  tax_no   number(10),
-  dname    varchar(25)
+  tax_no   number(10)	NOT NULL,
+  dname    varchar(25),
+FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no) ON DELETE CASCADE,
+PRIMARY KEY (tax_no)
 );
 
-ALTER TABLE PERSON
-	ADD CONSTRAINT  XPKPERSON PRIMARY KEY (sno);
-	
-ALTER TABLE DOCTOR
-	ADD (CONSTRAINT  is_a_p1 FOREIGN KEY (sno) REFERENCES PERSON(sno) ON DELETE SET NULL);
-ALTER TABLE DOCTOR
-	ADD CONSTRAINT  XPKDOCTOR PRIMARY KEY (sno);
+CREATE TABLE TRANSA (
+  trans_id number(10),
+  tax_no_dep   number(10),
+  tax_no_ph number(10),  
+  amount  number(3),
+  time_of_trans date,
+  PRIMARY KEY (tax_no_dep, tax_no_ph, trans_id),
+  FOREIGN KEY (tax_no_dep) REFERENCES DEPOT(tax_no),
+  FOREIGN KEY (tax_no_ph) REFERENCES PHARMACY(tax_no)
 
-ALTER TABLE PRESCRIPTION
-	ADD (CONSTRAINT  p2u FOREIGN KEY (sno) REFERENCES DOCTOR(sno));
-	
-ALTER TABLE PRESCRIPTION
-	ADD CONSTRAINT  XPKPRESCRIPTION PRIMARY KEY (sno, pres_no);
+);
 
-ALTER TABLE MEDICINE
-	ADD CONSTRAINT  XPKMEDICINE PRIMARY KEY (bcode);
+CREATE SEQUENCE trans_seq START WITH 1;
 
-ALTER TABLE ATC_AUTH
-	ADD (CONSTRAINT  atc4d FOREIGN KEY (sno) REFERENCES DOCTOR(sno) ON DELETE SET NULL);
-ALTER TABLE ATC_AUTH
-	ADD CONSTRAINT  XPKATCAUTH PRIMARY KEY (sno, atc_code);
-	
-ALTER TABLE CONTENT
-	ADD (CONSTRAINT  con4mdr FOREIGN KEY (sno) REFERENCES DOCTOR(sno));
-ALTER TABLE CONTENT
-	ADD (CONSTRAINT  pres4md FOREIGN KEY (pres_no) REFERENCES PRESCRIPTION(pres_no) ON DELETE SET NULL);
-ALTER TABLE CONTENT
-	ADD (CONSTRAINT  code4con FOREIGN KEY (bcode) REFERENCES MEDICINE(bcode));
-ALTER TABLE CONTENT 
-	ADD CONSTRAINT  XPKCONTENT PRIMARY KEY (sno, pres_no, bcode);
-	
-ALTER TABLE HEALTH_INSTITUTION
-	ADD CONSTRAINT  XPKHEALTHINS PRIMARY KEY (tax_no);
-
-ALTER TABLE PHARMACIST
-	ADD (CONSTRAINT  ph4tax FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no) ON DELETE SET NULL);
-ALTER TABLE PHARMACIST
-	ADD (CONSTRAINT  ph4sno FOREIGN KEY (sno) REFERENCES PERSON(sno) ON DELETE SET NULL);
-ALTER TABLE PHARMACIST
-	ADD CONSTRAINT  XPKPHSNO PRIMARY KEY (sno);
-	
-
-ALTER TABLE PHARMACY
-	ADD (CONSTRAINT  ph4ph FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no));
-ALTER TABLE PHARMACY
-	ADD CONSTRAINT  XPKPHARMACY PRIMARY KEY (tax_no);
-
-
-ALTER TABLE INVENTORY
-	ADD (CONSTRAINT  tax4ph FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no));
-ALTER TABLE INVENTORY
-	ADD (CONSTRAINT  bc4ph FOREIGN KEY (bcode) REFERENCES MEDICINE(bcode));
-ALTER TABLE INVENTORY
-	ADD CONSTRAINT  XPKINVENTORY PRIMARY KEY (tax_no, bcode);
-	
-ALTER TABLE DEPOT
-	ADD (CONSTRAINT  dp4dp FOREIGN KEY (tax_no) REFERENCES HEALTH_INSTITUTION(tax_no));
-ALTER TABLE DEPOT
-	ADD CONSTRAINT  XPKDEPOT PRIMARY KEY (tax_no);
-
+CREATE OR REPLACE TRIGGER trans_trig 
+BEFORE INSERT ON TRANSA 
+FOR EACH ROW
+BEGIN
+  SELECT trans_seq.NEXTVAL
+  INTO   :new.trans_id
+  FROM   dual;
+END;
 
 
 
