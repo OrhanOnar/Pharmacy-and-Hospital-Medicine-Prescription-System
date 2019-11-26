4abf99c5d8d9634a3a5bce31e8ee39260e13ee0d
abozgungordu <abozgungordu@gmail.com>
2019-11-26 05:30:26 +0300

    Fixed and added some queries , views and web page codes

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request, flash, redirect ,url_for
+from flask import Flask, render_template, request, redirect ,url_for
 from forms import LoginForm
-from database import find_sno, is_Doctor, find_pw, is_Pharmacist, is_Patient
+from database import find_sno, is_Doctor, find_pw, is_Pharmacist, is_Patient, patient_history, find_name, prescription
 import os
 from flask_login import login_user , LoginManager, login_required
 from user import User
 
+
 app = Flask(__name__)
+
+
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config["SQLALCHEMY_ECHO"] = True
 login = LoginManager(app)
 login.login_view = 'login'
 
+@app.route('/contact-us/',methods=['GET','POST'])
+def contact_us():
+
+    return render_template('contact-us.html')
+
+@app.route('/bildirim/',methods=['GET','POST'])
+def bildirim():
+
+    return render_template('bildirim.html')
 
 @login.user_loader
 def load_user(sno):
@@ -19,9 +31,9 @@ def load_user(sno):
 
 
 @app.route('/',methods=['GET','POST'])
-def home():
+def index():
 
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -40,26 +52,43 @@ def login():
             elif is_Patient(sno):
                 return redirect(url_for('patient', user_id=username))
             elif is_Pharmacist(sno):
-                return redirect(url_for('pharmacist', user_id=username))
+                return redirect(url_for('receteNoGiris', user_id=username))
     return render_template('login.html', form=form)
 
 @login_required
-@app.route('/doctor/<user_id>',methods=['GET','POST'])
+@app.route('/DoktorEkranı/<user_id>',methods=['GET','POST'])
 def doctor(user_id):
 
-    return render_template('doctor.html')
+    return render_template('DoktorEkranı.html')
 
 @login_required
-@app.route('/patient/<user_id>',methods=['GET','POST'])
+@app.route('/Hasta ekranı/<user_id>',methods=['GET','POST'])
 def patient(user_id):
+    x=""
 
-    return render_template('patient.html')
+    dict_patient=patient_history(user_id)
+    x = "DOKTOR : " + find_name(dict_patient[0]['sno_doc']) + "\n" + "HASTA : " + find_name(
+        dict_patient[0]['sno_pat']) + "\n" + "REÇETE NUMARASI : " + str(dict_patient[0]['pres_no']) + "\n" + "TARİHİ : " + str(
+        dict_patient[0]['expire']) + "\n" + "GEÇERLİLİĞİ : " + str(dict_patient[0]['provision']) + "\n" + "İLAÇ : " + dict_patient[0]['kind']
+    name=find_name(user_id)
+
+    return render_template('Hasta ekranı.html',name=name, patient=x)
 
 @login_required
-@app.route('/pharmacist/<user_id>',methods=['GET','POST'])
-def pharmacist(user_id):
+@app.route('/receteNoGiris/<user_id>/',methods=['GET','POST'])
+def receteNoGiris(user_id):
+    x=""
 
-    return render_template('pharmacist.html')
+    if request.method == 'POST':
+        pres_no = str(request.form.get('recete_no'))
+        pres = prescription(pres_no)
+        x="DOKTOR : " +find_name(pres[0]['sno_doc']) + "\n" + "HASTA : " +find_name(pres[0]['sno_pat']) + "\n"  + "REÇETE NUMARASI : " + str(pres[0]['pres_no']) + "\n" + "TARİHİ : " + str(pres[0]['expire'])+ "\n"  + "GEÇERLİLİĞİ : " + str(pres[0]['provision'])+ "\n"  + "İLAÇ : " + pres[0]['kind']
+
+
+    return render_template('receteNoGiris.html',user_id=user_id,prescript=x)
+
+
+
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -14,21 +14,19 @@ engine = create_engine(ENGINE_PATH_WIN_AUTH)
 
 
 def find_sno(usersno):
-
     with engine.connect() as con:
-        username = con.execute('SELECT "sno" FROM PERSON WHERE "sno"=:no',no=usersno)
+        username = con.execute('SELECT "sno" FROM PERSON WHERE "sno"=:no', no=usersno)
         resultset = []
         for row in username:
             resultset.append(dict(row))
-        result=resultset[0]['sno']
-
+        result = resultset[0]['sno']
 
     return result
 
-def find_pw(usersno):
 
+def find_pw(usersno):
     with engine.connect() as con:
-        password = con.execute('SELECT "pass" FROM PERSON WHERE "sno"=:no',no=usersno)
+        password = con.execute('SELECT "pass" FROM PERSON WHERE "sno"=:no', no=usersno)
         resultset = []
         for row in password:
             resultset.append(dict(row))
@@ -36,9 +34,10 @@ def find_pw(usersno):
 
     return result
 
+
 def is_Doctor(usersno):
     with engine.connect() as con:
-        username = con.execute('SELECT "sno" FROM DOCTOR WHERE "sno"=:no',no=usersno)
+        username = con.execute('SELECT "sno" FROM DOCTOR WHERE "sno"=:no', no=usersno)
         resultset = []
         for row in username:
             resultset.append(dict(row))
@@ -46,10 +45,11 @@ def is_Doctor(usersno):
             return 1
         else:
             return 0
+
 
 def is_Patient(usersno):
     with engine.connect() as con:
-        username = con.execute('SELECT "sno" FROM PATIENT WHERE "sno"=:no',no=usersno)
+        username = con.execute('SELECT "sno" FROM PATIENT WHERE "sno"=:no', no=usersno)
         resultset = []
         for row in username:
             resultset.append(dict(row))
@@ -57,10 +57,11 @@ def is_Patient(usersno):
             return 1
         else:
             return 0
+
 
 def is_Pharmacist(usersno):
     with engine.connect() as con:
-        username = con.execute('SELECT "sno" FROM PHARMACIST WHERE "sno"=:no',no=usersno)
+        username = con.execute('SELECT "sno" FROM PHARMACIST WHERE "sno"=:no', no=usersno)
         resultset = []
         for row in username:
             resultset.append(dict(row))
@@ -68,79 +69,100 @@ def is_Pharmacist(usersno):
             return 1
         else:
             return 0
-
 
 
 def all_sno():
     with engine.connect() as con:
-        username=con.execute('SELECT * FROM PERSON')
+        username = con.execute('SELECT * FROM PERSON')
 
     return username
 
+
 def patient_history(patientno):
     with engine.connect() as con:
-        history = con.execute('SELECT * FROM PATIENT P, PRESCRIPTION PRE, CONTENT C WHERE P."sno" = PRE."sno_pat" AND PRE."pres_no" = C."pres_no" AND P."sno"=:no GROUP BY P."pres_no"',no=patientno)
+        history = con.execute('SELECT * FROM PRESCRIPTION WHERE "sno_pat"=:no', no=patientno)
         resultset = []
         for row in history:
             resultset.append(dict(row))
         return resultset
-    
-def check_doctor_auth(doctorno):
+
+
+def find_name(usersno):
     with engine.connect() as con:
-        auth = con.execute('SELECT AC."mname", AC."atc_name" FROM ACTIVE_INGREDIENT AC WHERE AC."atc_code" IN ( SELECT "atc_code" FROM ATC_AUTH AA, DOCTOR D WHERE AA."branch" = D."branch" AND D."sno" =:no)',no=doctorno)
+        name = con.execute('SELECT "fname","lname" FROM PERSON WHERE "sno"=:no', no=usersno)
         resultset = []
-        for row in auth:
+        for row in name:
+            resultset.append(dict(row))
+        result = resultset[0]['fname'] + " " + resultset[0]['lname']
+
+    return result
+
+
+def prescription(pres_no):
+    with engine.connect() as con:
+        history = con.execute('SELECT * FROM PRESCRIPTION WHERE "pres_no"=:no', no=pres_no)
+        resultset = []
+        for row in history:
             resultset.append(dict(row))
         return resultset
 
-def depot_inv_check(depot_tax_no):
-    with engine.connect() as con:
-        inv = con.execute('SELECT M."mname", H."build_name" FROM MEDICINE M, DEPOT D, INVENTORY I, HEALTH_INSTITUTION H WHERE  D."tax_no" = I."tax_no" AND M."bcode" = I."bcode" AND H."tax_no"=D."tax_no" SORT BY M."mname" ASC')
-        resultset = []
-        for item in inv:
-            resultset.append(dict(item))
-        return resultset
+
+#def check_doctor_auth(doctorno):
+#    with engine.connect() as con:
+#        auth = con.execute('SELECT AC."mname", AC."atc_name" FROM ACTIVE_INGREDIENT AC WHERE AC."atc_code" IN ( SELECT "atc_code" FROM ATC_AUTH AA, DOCTOR D WHERE AA."branch" = D."branch" AND D."sno" =:no)',no=doctorno)
+#        resultset = []
+#        for row in auth:
+#            resultset.append(dict(row))
+#        return resultset
+
+#def depot_inv_check(depot_tax_no):
+#    with engine.connect() as con:
+#        inv = con.execute('SELECT M."mname", H."build_name" FROM MEDICINE M, DEPOT D, INVENTORY I, HEALTH_INSTITUTION H WHERE  D."tax_no" = I."tax_no" AND M."bcode" = I."bcode" AND H."tax_no"=D."tax_no" SORT BY M."mname" ASC')
+#        resultset = []
+#        for item in inv:
+#            resultset.append(dict(item))
+#        return resultset
     
-def depot_inv_search(med_name):
-    with engine.connect() as con:
-        meds = con.execute('SELECT M."mname", H."build_name" FROM MEDICINE M, DEPOT D, INVENTORY I, HEALTH_INSTITUTION H WHERE H."tax_no" + D."tax_no" AND D."tax_no" = I."tax_no" AND M."bcode" = I."bcode" AND M."mname" = :medm GROUP BY M."mname" ASC', medm=med_name)
-        return meds
+#def depot_inv_search(med_name):
+#    with engine.connect() as con:
+#        meds = con.execute('SELECT M."mname", H."build_name" FROM MEDICINE M, DEPOT D, INVENTORY I, HEALTH_INSTITUTION H WHERE H."tax_no" + D."tax_no" AND D."tax_no" = I."tax_no" AND M."bcode" = I."bcode" AND M."mname" = :medm GROUP BY M."mname" ASC', medm=med_name)
+#        return meds
     
-def create_prescription(doctorno, patientno, bcode, amount):
-    with engine.connect() as con:
-        con.execute('INSERT INTO PRESCRIPTION (sno_doc, sno_pat, pres_no, expire, provision) VALUES (:sno1, :sno2, pres_seq.nextval,  DATEADD(month, 3, (SELECT LOCALTIMESTAMP FROM dual)), (SELECT LOCALTIMESTAMP FROM dual))', sno1=doctorno, sno2=patientno)
-        for i in range(0, len(bcode)):
-            con.execute('INSERT INTO CONTENT (pres_no, bcode, amount) VALUES (pres_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i]) 
-        return
+#def create_prescription(doctorno, patientno, bcode, amount):
+#    with engine.connect() as con:
+#        con.execute('INSERT INTO PRESCRIPTION (sno_doc, sno_pat, pres_no, expire, provision) VALUES (:sno1, :sno2, pres_seq.nextval,  DATEADD(month, 3, (SELECT LOCALTIMESTAMP FROM dual)), (SELECT LOCALTIMESTAMP FROM dual))', sno1=doctorno, sno2=patientno)
+#        for i in range(0, len(bcode)):
+#            con.execute('INSERT INTO CONTENT (pres_no, bcode, amount) VALUES (pres_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i]) 
+#        return
     
- def prescription_check(pres_num):
-    with engine.connect() as con:
-        result = con.execute('SELECT * FROM PATIENT P, PRESCRIPTION PR ,CONTENT C,  WHERE P."sno" = PR."sno_pat"  PR."pres_no" = C."pres_no" AND PR."pres_no" =:pres_num GROUP BY "pres_no"', pres_num=pres_no)
-        return result 
+# def prescription_check(pres_num):
+#    with engine.connect() as con:
+#        result = con.execute('SELECT * FROM PATIENT P, PRESCRIPTION PR ,CONTENT C,  WHERE P."sno" = PR."sno_pat"  PR."pres_no" = C."pres_no" AND PR."pres_no" =:pres_num GROUP BY "pres_no"', pres_num=pres_no)
+#        return result 
   
-def pharmacy_inv_check(pharmacist_num):
-    with engine.connect() as con:
-        result = con.execute('SELECT M."bcode", M."mname", I."amount" FROM PHARMACIST P, INVENTORY I, MEDICINE M WHERE P."tax_no" = I."tax_no" AND I."bcode" = M."bcode" AND P."sno" =:psno', psno=pharmacist_num)
-        return result
+#def pharmacy_inv_check(pharmacist_num):
+#    with engine.connect() as con:
+#        result = con.execute('SELECT M."bcode", M."mname", I."amount" FROM PHARMACIST P, INVENTORY I, MEDICINE M WHERE P."tax_no" = I."tax_no" AND I."bcode" = M."bcode" AND P."sno" =:psno', psno=pharmacist_num)
+#        return result
   
 
-def pharmacy_inv_med_search(pharmacist_num, med_name):
-    with engine.connect() as con:
-        result = con.execute('SELECT M."bcode", M."mname", I."amount" FROM PHARMACIST P, INVENTORY I, MEDICINE M WHERE P."tax_no" = I."tax_no" AND I."bcode" = M."bcode" AND M."mname"=:medm AND P."sno" =:psno', medm=med_name, psno=pharmacist_num)
-        return result
+#def pharmacy_inv_med_search(pharmacist_num, med_name):
+#    with engine.connect() as con:
+#        result = con.execute('SELECT M."bcode", M."mname", I."amount" FROM PHARMACIST P, INVENTORY I, MEDICINE M WHERE P."tax_no" = I."tax_no" AND I."bcode" = M."bcode" AND M."mname"=:medm AND P."sno" =:psno', medm=med_name, psno=pharmacist_num)
+#        return result
     
-def create_receipt(phar_tax_no, patient_no, bcode, amount):
-    with engine.connect() as con:
-        con.execute('INSERT INTO RECEIPT ("phar_tax_no", "pat_sno", "receipt_id") VALUES (:ptax, :pno, rec_seq.nextval)', ptax=phar_tax_no, pno=patient_no)
-        for i in range(0, len(bcode)):
-            con.execute('INSERT INTO RECEIPT ("receipt_id", "bcode", "amount") VALUES (rec_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i])
-            con.execute('UPDATE INVENTORY I SET amount = (amount - :amou) WHERE I."tax_no" =:ptax AND I."bcode" =:bc', ptax=phar_tax_no, bc=bcode[i], amou=amount[i])
-        return
+#def create_receipt(phar_tax_no, patient_no, bcode, amount):
+#    with engine.connect() as con:
+#        con.execute('INSERT INTO RECEIPT ("phar_tax_no", "pat_sno", "receipt_id") VALUES (:ptax, :pno, rec_seq.nextval)', ptax=phar_tax_no, pno=patient_no)
+#        for i in range(0, len(bcode)):
+#            con.execute('INSERT INTO RECEIPT ("receipt_id", "bcode", "amount") VALUES (rec_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i])
+#            con.execute('UPDATE INVENTORY I SET amount = (amount - :amou) WHERE I."tax_no" =:ptax AND I."bcode" =:bc', ptax=phar_tax_no, bc=bcode[i], amou=amount[i])
+#        return
     
-def create_transaction(depot_tax_no, pharmacy_tax_no, bcode, amount):
-    with.engine.connect() as con:
-        con.execute('INSERT INTO TRANSA ("trans_id", "tax_no_dep", "tax_no_ph", "time_of_trans") VALUES (trans_seq.nextval, :dtax, :ptax, (SELECT LOCALTIMESTAMP FROM dual))', dtax=depot_tax_no, ptax=pharmacy_tax_no)
-        for i in range(0, len(bcode)):
-            con.execute('INSERT INTO TRANSA_CONTENT (trans_id, bcode, amount) VALUES (trans_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i])
-            con.execute('UPDATE INVENTORY I SET amount = (amount - :amou) WHERE I."tax_no" =:ptax AND I."bcode" =:bc', bc=bcode[i], amou=amount[i])
-        return
+#def create_transaction(depot_tax_no, pharmacy_tax_no, bcode, amount):
+#    with.engine.connect() as con:
+#        con.execute('INSERT INTO TRANSA ("trans_id", "tax_no_dep", "tax_no_ph", "time_of_trans") VALUES (trans_seq.nextval, :dtax, :ptax, (SELECT LOCALTIMESTAMP FROM dual))', dtax=depot_tax_no, ptax=pharmacy_tax_no)
+#        for i in range(0, len(bcode)):
+#            con.execute('INSERT INTO TRANSA_CONTENT (trans_id, bcode, amount) VALUES (trans_seq.currval, :bc, :amou)', bc=bcode[i], amou=amount[i])
+#            con.execute('UPDATE INVENTORY I SET amount = (amount - :amou) WHERE I."tax_no" =:ptax AND I."bcode" =:bc', bc=bcode[i], amou=amount[i])
+#        return
